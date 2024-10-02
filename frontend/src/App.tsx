7ea18d9355602b84873aa064fe6a1e9d18eafd09
Joel Vitor Niino <joelprofessor13@gmail.com>
2024-10-02 15:35:28 -0300

    feature: add profile on router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Register from './pages/Register'
 import Login from './pages/Login';
