b502a22979a35211c38a672180153d8abf2dbb45
Joel Vitor Niino <joelprofessor13@gmail.com>
2024-10-02 13:46:28 -0300

    feature: app route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/authRoutes';
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (request: express.Request, response: express.Response) => {
-    response.send("Hello, World!");
-});
-
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
