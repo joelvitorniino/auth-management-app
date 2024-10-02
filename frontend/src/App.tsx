a97d86e4239c11e002fd0d86dc3de56b8c2a9480
Joel Vitor Niino <joelprofessor13@gmail.com>
2024-10-02 13:25:31 -0300

    feature: profile route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Register from './pages/Register'
 import Login from './pages/Login';
+import Profile from './pages/Profile';
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/profile" element={<Profile />} />
             </Routes>
         </Router>
     );
