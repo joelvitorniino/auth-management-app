45cf93947ca5ec2907b9fc001b9a809ef992dc2b
Joel Vitor Niino <joelprofessor13@gmail.com>
2024-10-02 15:36:13 -0300

    feature: cors initialized

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
