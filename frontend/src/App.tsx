a3a7c563ed294dabc2255a8da928f527beb37ef2
Joel Vitor Niino <joelprofessor13@gmail.com>
2024-10-02 13:19:15 -0300

    feature: login router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Register from './pages/Register'
+import Login from './pages/Login';
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
         </Router>
     );
